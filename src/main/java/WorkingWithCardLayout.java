be92e63a950c5c8a136e05ca58372a36e137ab75
austin wamono <austinwamono98@gmail.com>
2025-09-11 01:08:10 +0300

    commit#2:created the method to use to display the JFrame

--- src/main/java/WorkingWithCardLayout.java
+++ src/main/java/WorkingWithCardLayout.java
@@ -3,4 +3,15 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class WorkingWithCardLayout {
+    JFrame mainframe;
+    public WorkingWithCardLayout() {
+        showMainFrame();
+    }
+    //creating a method instead of using the constructor
+    public void showMainFrame(){
+        mainframe=new JFrame("working with cardlayout");
+        mainframe.setSize(500,700);
+        //CardLayout card=new CardLayout();
+        mainframe.setVisible(true);
+    };
 }

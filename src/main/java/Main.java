511c137e95ab7edd184a7a47b3955a6e34d11794
austin wamono <austinwamono98@gmail.com>
2025-09-11 11:33:55 +0300

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
 public class Main{
     public static void main(String[]args){
         WorkingWithCardLayout cards=new WorkingWithCardLayout();
-        //commented it out to avoid it appearing twice its purpose is just for testing if the jframe works
-        //cards.showMainFrame();
+        cards.showMainFrame();
         cards.showcards();
 
 

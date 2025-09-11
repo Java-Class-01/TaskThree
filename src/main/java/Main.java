49e6f7de4893a06d85d36faf142e91903f6fbcc9
austin wamono <austinwamono98@gmail.com>
2025-09-11 10:57:14 +0300

    commented out the method showMainFrame() since it was for testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 public class Main{
     public static void main(String[]args){
         WorkingWithCardLayout cards=new WorkingWithCardLayout();
-        cards.showMainFrame();
+        //commented it out to avoid it appearing twice its purpose is just for testing if the jframe works
+        //cards.showMainFrame();
         cards.showcards();
 
 
